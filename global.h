82a028a492a345623a1341a2236bf5d853dc78f6
dellamoresteven <dellamoresteven@gmail.com>
2020-12-13 15:24:12 -0600

    told people where to edit

--- global.h
+++ global.h
@@ -1,12 +1,12 @@
 namespace global {
-    int W = 0; // W of resized frame
-    int H = 0; // H of resized frame
-    int real_W = 0; // The OG W of the frame
-    int real_H = 0; // The OG H of the frame
-    int start_W = 1660; // The start width of the minimap
-    int start_H = 820; // The start height of the minimap
-    int max_W = 1536; // The max display allowed. Will scale down if we have too
-    int start_frame = 110000; // Frame to start on
-    int num_frames = 30; // Number of frames to process and render
+    int W               = 0; // W of resized frame 
+    int H               = 0; // H of resized frame
+    int real_W          = 0; // The OG W of the frame
+    int real_H          = 0; // The OG H of the frame
+    int start_W         = 1660; // The start width of the minimap   ***EDIT***
+    int start_H         = 820; // The start height of the minimap   ***EDIT***
+    int max_W           = 1536; // The max display allowed. Will scale down if we have too   ***EDIT***
+    int start_frame     = 110000; // Frame to start on   ***EDIT***
+    int num_frames      = 30; // Number of frames to process and render   ***EDIT***
 }
 
